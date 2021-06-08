0ace1d116f22177673d222876e7f9303e65ceffb
Nipuna Weerasekara <w.nipuna@gmail.com>
2021-06-08 12:11:29 +0530

    for testing comment line 41 /app.js

--- app.js
+++ app.js
@@ -1,63 +1,64 @@
-var express = require('express');
-var session = require('express-session')
-var engine = require('ejs-locals');
-var path = require('path');
-var favicon = require('serve-favicon');
+var express = require("express");
+var session = require("express-session");
+var engine = require("ejs-locals");
+var path = require("path");
+var favicon = require("serve-favicon");
 var fs = require("fs");
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+var logger = require("morgan");
+var cookieParser = require("cookie-parser");
+var bodyParser = require("body-parser");
 var log4js = require("log4js");
 
-var init_db = require('./model/init_db');
-var login = require('./routes/login');
-var products = require('./routes/products');
+var init_db = require("./model/init_db");
+var login = require("./routes/login");
+var products = require("./routes/products");
 
 var app = express();
 
 // config second logger
-log4js.loadAppender('file');
+log4js.loadAppender("file");
 //log4js.addAppender(log4js.appenders.console());
-log4js.addAppender(log4js.appenders.file('app-custom.log'), 'vnode');
+log4js.addAppender(log4js.appenders.file("app-custom.log"), "vnode");
 
-var logger4js = log4js.getLogger('vnode');
-logger4js.setLevel('INFO');
+var logger4js = log4js.getLogger("vnode");
+logger4js.setLevel("INFO");
 
-var accessLogStream = fs.createWriteStream(path.join(__dirname, 'access.log'))
+var accessLogStream = fs.createWriteStream(path.join(__dirname, "access.log"));
 
 /*
  * Template engine
  */
-app.engine('ejs', engine);
+app.engine("ejs", engine);
 
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
 // uncomment after placing your favicon in /public
-app.use(logger('combined', {stream: accessLogStream}));
+app.use(logger("combined", { stream: accessLogStream }));
 app.use(bodyParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(session({
-  secret: 'ñasddfilhpaf78h78032h780g780fg780asg780dsbovncubuyvqy',
-  cookie: {
-    secure: false,
-    maxAge: 99999999999
-  }
-}));
+// app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
+app.use(
+  session({
+    secret: "ñasddfilhpaf78h78032h780g780fg780asg780dsbovncubuyvqy",
+    cookie: {
+      secure: false,
+      maxAge: 99999999999,
+    },
+  })
+);
 
 /*
  * Routes config
  */
-app.use('', products);
-app.use('', login);
-
+app.use("", products);
+app.use("", login);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
+app.use(function (req, res, next) {
+  var err = new Error("Not Found");
   err.status = 404;
   next(err);
 });
@@ -65,30 +66,30 @@ app.use(function(req, res, next) {
 /*
  * Debug functions and error handlers
  */
-if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
+if (app.get("env") === "development") {
+  app.use(function (err, req, res, next) {
     res.status(err.status || 500);
-    res.render('error', {
+    res.render("error", {
       message: err.message,
-      error: err
+      error: err,
     });
   });
 }
 
 // production error handler
 // no stacktraces leaked to user
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   res.status(err.status || 500);
-  res.render('error', {
+  res.render("error", {
     message: err.message,
-    error: {}
+    error: {},
   });
 });
 
 /*
  * Create database
  */
-logger4js.info("Building database")
+logger4js.info("Building database");
 // logger.info(("Building database");
 
 init_db();

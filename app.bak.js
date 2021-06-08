d78417f673ac7cf3611ab622c46b46c5874d1850
Nipuna Weerasekara <w.nipuna@gmail.com>
2021-06-08 12:31:03 +0530

    refactor app.js to app.bak.js

--- app.bak.js
+++ app.bak.js
@@ -38,7 +38,7 @@ app.use(logger("combined", { stream: accessLogStream }));
 app.use(bodyParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-// app.use(cookieParser());
+app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 app.use(
   session({

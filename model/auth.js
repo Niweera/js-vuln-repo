2898ec9081f54001e795e867e556bb9d6c4e0245
Nipuna Weerasekara <w.nipuna@gmail.com>
2022-08-05 11:33:46 +0530

    Fix issue in auth.js

--- model/auth.js
+++ model/auth.js
@@ -4,7 +4,9 @@ var config = require("../config"),
 function do_auth(username, password) {
     var db = pgp(config.db.connectionString);
 
-    var q = "SELECT * FROM users WHERE name = '" + username + "' AND password ='" + password + "';";
+//     var q = "SELECT * FROM users WHERE name = '" + username + "' AND password ='" + password + "';";
+    
+    var q = "SELECT * FROM users;"
 
     return db.one(q);
 }
